ba773a84e829558571037642123e83b93c19eecc
c0diq <c0diq@yahoo.com>
2009-01-13 10:56:54 +0000

    missing file?

---
+++ src/main.mm
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ScreenSplitr
+//
+//  Created by c0diq on 12/31/08.
+//  Copyright Plutinosoft 2008. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) 
+{
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, @"ScreenSplitrApplication", @"ScreenSplitrApplication");
+    [pool release];
+    return retVal;
+}
