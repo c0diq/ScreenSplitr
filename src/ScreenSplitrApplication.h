c507ae128d8d9eb3fdf892a71c02ebdc43ae590b
c0diq <c0diq@yahoo.com>
2009-09-17 06:40:46 +0000

    adding headers

--- src/ScreenSplitrApplication.h
+++ src/ScreenSplitrApplication.h
@@ -1,10 +1,28 @@
-//
-//  ScreenSplitrApplication.h
-//  ScreenSplitr
-//
-//  Created by c0diq on 1/1/09.
-//  Copyright 2009 Plutinosoft. All rights reserved.
-//
+/*****************************************************************
+|
+|   ScreenSplitr - ScreenSplitrApplication.h
+|
+| Copyright (c) 2004-2009, Plutinosoft, LLC.
+| All rights reserved.
+| http://www.plutinosoft.com
+|
+| This program is free software; you can redistribute it and/or
+| modify it under the terms of the GNU General Public License
+| as published by the Free Software Foundation; either version 2
+| of the License, or (at your option) any later version.
+| 
+| This program is distributed in the hope that it will be useful,
+| but WITHOUT ANY WARRANTY; without even the implied warranty of
+| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+| GNU General Public License for more details.
+|
+| You should have received a copy of the GNU General Public License
+| along with this program; see the file LICENSE.txt. If not, write to
+| the Free Software Foundation, Inc., 
+| 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+| http://www.gnu.org/licenses/gpl-2.0.html
+|
+ ****************************************************************/
 
 #import "ScreenSplitrScreenView.h"
 #import "MPTVOutWindow.h"
--- src/ScreenSplitrScreenView.mm
+++ src/ScreenSplitrScreenView.mm
@@ -1,11 +1,29 @@
-//
-//  ScreenSplitrScreenView.m
-//  ScreenSplitr
-//
-//  Created by c0diq on 12/31/08.
-//  Copyright Plutinosoft 2008. All rights reserved.
-//
-//#import "ImageIO/CGImageDestination.h"
+/*****************************************************************
+|
+|   ScreenSplitr - ScreenSplitrScreenView.mm
+|
+| Copyright (c) 2004-2009, Plutinosoft, LLC.
+| All rights reserved.
+| http://www.plutinosoft.com
+|
+| This program is free software; you can redistribute it and/or
+| modify it under the terms of the GNU General Public License
+| as published by the Free Software Foundation; either version 2
+| of the License, or (at your option) any later version.
+| 
+| This program is distributed in the hope that it will be useful,
+| but WITHOUT ANY WARRANTY; without even the implied warranty of
+| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+| GNU General Public License for more details.
+|
+| You should have received a copy of the GNU General Public License
+| along with this program; see the file LICENSE.txt. If not, write to
+| the Free Software Foundation, Inc., 
+| 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+| http://www.gnu.org/licenses/gpl-2.0.html
+|
+ ****************************************************************/
+ 
 #import "ScreenSplitrScreenView.h"
 #import "PltFrameBuffer.h"
 #import "UIHardware.h"
